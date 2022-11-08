2fc6be9039c2d7381b580e6e0772fed6061cbf65
TejeshwarSingh <singh.tejeshwar@gmail.com>
2022-11-08 13:10:20 -0600

    Ch12 Controller
    Ch12 Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+//import { AppController } from './app.controller'; // removed as files are deleted
+//import { AppService } from './app.service';       // removed as files are deleted
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TasksModule],
+ // controllers: [AppController], // removed as files are deleted
+ // providers: [AppService], // removed as files are deleted
 })
 export class AppModule {}
